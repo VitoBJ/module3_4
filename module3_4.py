a12f18d32cee441f6a75f9ac9c43b9777724e23e
VitoBJ <Shizane7@gmail.com>
2024-08-10 20:30:18 +0300

    Add files via upload

---
+++ module3_4.py
@@ -0,0 +1,14 @@
+def single_root_words(root_word, *other_words):
+    same_words = []
+    root_word_lower = root_word.lower()
+    for word in other_words:
+
+        word_lower = word.lower()
+
+        if root_word_lower in word_lower or word_lower in root_word_lower:
+            same_words.append(word)
+
+    return same_words
+
+result = single_root_words('суп', 'Суповой набо', 'макароны', 'ЛУК', 'СуП', 'СУПЧИК')
+print(result)
